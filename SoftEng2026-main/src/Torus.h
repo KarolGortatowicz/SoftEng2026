5f8af5e439b81f0ff6487424bfbb30d4b9d01647
Karol Gortatowicz <gortatwiczkarol@gmail.com>
2026-03-07 21:20:33 +0100

    Implemented torus

---
+++ SoftEng2026-main/src/Torus.h
@@ -0,0 +1,49 @@
+#ifndef _TORUS_H
+#define _TORUS_H
+
+
+#include "Shape3D.h"
+#include "ShapeResultData.h"
+#include <string>
+using namespace std;
+#include "ShapeParam.h"
+
+template<class T>
+class Torus : public Shape3D<T> {
+  public:
+    inline virtual ShapeResultData<T> compute();
+
+    inline string print();
+
+    inline Torus(const ShapeParam<T> & param);
+
+};
+template<class T>
+inline ShapeResultData<T> Torus<T>::compute(){
+
+    ShapeResultData<T> result;
+
+    const T PI = 3.141592653589793;
+
+    T R = this->m_param.get(PARAM_RADIUS);
+    T r = this->m_param.get(PARAM_WIDTH);
+
+    T surface = 4 * PI * PI * R * r;
+    T volume = 2 * PI * PI * R * r * r;
+
+    result.set(RESULT_SURFACE, surface);
+    result.set(RESULT_VOLUME, volume);
+
+    return result;
+}
+
+template<class T>
+inline string Torus<T>::print() {
+  return "";
+}
+
+template<class T>
+inline Torus<T>::Torus(const ShapeParam<T> & param) : Shape3D<T>(param) {
+}
+
+#endif
